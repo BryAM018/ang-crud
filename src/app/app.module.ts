fdfe1c7ad62615ed0994217755a66f79acd5e1cc
Bryan Anchundia <bjam1315074052@gmail.com>
2023-01-03 13:02:08 -0500

    loginx3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -46,6 +46,7 @@ const appRoutes:Routes=[
     AppRoutingModule,
     FormsModule,
     RouterModule.forRoot(appRoutes),
+    ReactiveFormsModule,
     HttpClientModule,
     NgbModalModule,
     BrowserAnimationsModule,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { laboratorio } from '../laboratorio/laboratorios.model';
 import { GestorComponent } from '../gestor/gestor.component';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  public myFrom!:FormGroup;
+  constructor(private fb:FormBuilder){
 
+  }
+ngOnInit():void{
+  this.myFrom=this.createMyFrom();
+}
+private createMyFrom():FormGroup{
+  return this.fb.group({
+    usuario:[],
+    password:[]
+  })
+}
 }
